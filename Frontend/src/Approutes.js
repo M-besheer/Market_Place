8eb4a08447ebfccd0cad2168b6b9ab6c6ecca94f
Abdelrahman <boudysallam1@gmail.com>
2026-04-15 22:33:59 +0200

    feat: go directly to /login

--- Frontend/src/Approutes.js
+++ Frontend/src/Approutes.js
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import OrderView from "./Views/Seller/OrderView";
 import Signup from "./Views/Authentication/Signup";
 import Login from "./Views/Authentication/Login";
+import { Navigate } from 'react-router-dom';
 
 function AppRoutes() {
     return (
         <Router>
             <Routes>
+            <Route path="/" element={<Navigate to="/login" replace />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/orders" element={<OrderView />} />
